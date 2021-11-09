426677eaabecf33b31009de832c1e6c470417eec
Ajaya Kumar Sharma <ajaya.sharma@outlook.com>
2021-11-09 16:17:56 +0545

    Debugging

--- lib/components/friend_post_list_view.dart
+++ lib/components/friend_post_list_view.dart
@@ -20,7 +20,7 @@ class FriendPostListView extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Social Chefts', style: Theme.of(context).textTheme.headline1),
+          Text('Social Chefs', style: Theme.of(context).textTheme.headline1),
           const SizedBox(height: 16),
           ListView.separated(
             itemBuilder: (context, index) {
