8e4238b1743e9f7f8f33e33d5bb0736953075e12
Ajaya Kumar Sharma <ajaya.sharma@outlook.com>
2021-11-09 14:27:01 +0545

    Listview Completed

--- lib/components/today_recipe_list_view.dart
+++ lib/components/today_recipe_list_view.dart
@@ -27,13 +27,33 @@ class TodayRecipeListView extends StatelessWidget {
           const SizedBox(height: 16),
           Container(
             height: 400,
-            // Todo: add listview here
-            color: Colors.grey,
+            color: Colors.transparent,
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                final recipe = recipes[index];
+                return buildCard(recipe);
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(width: 16);
+              },
+              itemCount: recipes.length,
+              scrollDirection: Axis.horizontal,
+            ),
           ),
         ],
       ),
     );
   }
 
-  // Todo: add buildCard() widget here
+  Widget buildCard(ExploreRecipe recipe) {
+    if (recipe.cardType == RecipeCardType.card1) {
+      return Card1(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card2) {
+      return Card2(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card3) {
+      return Card3(recipe: recipe);
+    } else {
+      throw Exception('This card doesn\'t exist yet');
+    }
+  }
 }
