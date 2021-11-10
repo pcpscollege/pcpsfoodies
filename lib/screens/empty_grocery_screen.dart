c1e60f5d6ed903a0078d6e312d52069ee40c30a5
Ajaya Kumar Sharma <ajaya.sharma@outlook.com>
2021-11-10 22:13:59 +0545

    Empty Grocery Screen Complete

--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -11,10 +11,39 @@ class EmptyGroceryScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // TODO: Add Empty Image
-            // Todo: Add empty screen title
-            // Todo: Add empty screen subtitle
-            // Todo: Add browse recipes button
+            // Add empty image
+            Flexible(
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Image.asset('assets/pcpsfoodies_assets/empty_list.png'),
+              ),
+            ),
+            // Add empty screen title
+            const Text(
+              'No Groceries',
+              style: TextStyle(fontSize: 21.0),
+            ),
+            // Add empty screen subtitle
+            const SizedBox(
+              height: 16.0,
+            ),
+            const Text(
+              'Shopping for ingredients?\n'
+              'Tap the + button to write them down!',
+              textAlign: TextAlign.center,
+            ),
+            // Add browse recipes button
+            MaterialButton(
+              textColor: Colors.white,
+              child: const Text('Browse Recipes'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              color: Colors.green,
+              onPressed: () {
+                // go to recipes tab
+              },
+            ),
           ],
         ),
       ),
