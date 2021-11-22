9a0f067b66161970665615bd1cf1ec19746ca934
Ajaya Kumar Sharma <ajaya.sharma@outlook.com>
2021-11-22 22:20:00 +0545

    completed form widgets

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 import '../models/models.dart';
+import '../components/grocery_tile.dart';
 
 class GroceryItemScreen extends StatefulWidget {
   // Add functions as the parameters
@@ -101,6 +102,22 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
             ),
             buildQuantityField(),
             // Add grocery tile
+            GroceryTile(
+              item: GroceryItem(
+                id: 'previewMode',
+                name: _name,
+                importance: _importance,
+                color: _currentColor,
+                quantity: _currentSliderValue,
+                date: DateTime(
+                  _dueDate.year,
+                  _dueDate.month,
+                  _dueDate.day,
+                  _timeOfDay.hour,
+                  _timeOfDay.minute,
+                ),
+              ),
+            ),
           ],
         ),
       ),
